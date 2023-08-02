6edff9fd27d9dd3a45f94440fb3a7c5a2172e82a
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-02 21:26:09 +0530

    created friend list

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,6 @@ import App from './App';
 import Loader from './Loader';
 import Navbar from './Navbar';
 import Comment from './Comment';
+import FriendsList from './FriendsList';
 
-
-
-
-export { App, Loader, Navbar , Comment};
+export { App, Loader, Navbar, Comment, FriendsList };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Comment, Loader } from '../components';
+import { Comment, Loader, FriendsList } from '../components';
 import styles from '../styles/home.module.css';
 import { useAuth } from '../hooks';
 import { getPosts } from '../api';
@@ -30,64 +30,67 @@ const Home = () => {
   }
 
   return (
-    <div className={styles.postsList}>
-      {/* to have multiple post */}
-      {posts.map((post) => (
-        <div className={styles.postWrapper} key={`post-${post._id}`}>
-          <div className={styles.postHeader}>
-            <div className={styles.postAvatar}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png"
-                alt="user-pic"
-              />
-              <div>
-                <Link
-                  to={{
-                    pathname: `/user/${post.user._id}`,
-                    state: {
-                      user: post.user,
-                    },
-                  }}
-                  className={styles.postAuthor}
-                >
-                  {post.user.name}
-                </Link>
-
-                <span className={styles.postTime}>a minute ago</span>
-              </div>
-            </div>
-            <div className={styles.postContent}>{post.conent}</div>
-
-            <div className={styles.postActions}>
-              <div className={styles.postLike}>
+    <div className={styles.home}>
+      <div className={styles.postsList}>
+        {/* to have multiple post */}
+        {posts.map((post) => (
+          <div className={styles.postWrapper} key={`post-${post._id}`}>
+            <div className={styles.postHeader}>
+              <div className={styles.postAvatar}>
                 <img
-                  src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                  alt="likes-icon"
+                  src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png"
+                  alt="user-pic"
                 />
-                <span>5</span>
+                <div>
+                  <Link
+                    to={{
+                      pathname: `/user/${post.user._id}`,
+                      state: {
+                        user: post.user,
+                      },
+                    }}
+                    className={styles.postAuthor}
+                  >
+                    {post.user.name}
+                  </Link>
+
+                  <span className={styles.postTime}>a minute ago</span>
+                </div>
+              </div>
+              <div className={styles.postContent}>{post.conent}</div>
+
+              <div className={styles.postActions}>
+                <div className={styles.postLike}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                    alt="likes-icon"
+                  />
+                  <span>5</span>
+                </div>
+
+                <div className={styles.postCommentsIcon}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/128/2190/2190552.png"
+                    alt="comments-icon"
+                  />
+                  <span>2</span>
+                </div>
               </div>
 
-              <div className={styles.postCommentsIcon}>
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/2190/2190552.png"
-                  alt="comments-icon"
-                />
-                <span>2</span>
+              <div className={styles.postCommentBox}>
+                <input placeholder="Start typing a comment" />
               </div>
-            </div>
 
-            <div className={styles.postCommentBox}>
-              <input placeholder="Start typing a comment" />
-            </div>
-
-            <div className={styles.postCommentsList}>
-              {post.comments.map((comment) => (
-                <Comment comment={comment} />
-              ))}
+              <div className={styles.postCommentsList}>
+                {post.comments.map((comment) => (
+                  <Comment comment={comment} />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      {auth.user && <FriendsList />}
     </div>
   );
 };
